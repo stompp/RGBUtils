4426dd7a0aebcbdb820978b06117481b47ba9a5d
J.M. Tomé <josemiguel.tomejimenez@gmail.com>
2021-12-03 19:27:25 +0100

    Removed processor defines, created periodic data types to test, created AnimationFunctions Class

--- color_animation.h
+++ color_animation.h
@@ -18,98 +18,211 @@
 
 // EFFECT MODES
 #define NO_ANIMATION 0
-#define MODE_BEATING 1
-#define MODE_RAINBOW1 2
-#define MODE_RAINBOW2 3
-#define MODE_FAST_RAINBOW 4
-#define MODE_CIRCLE_RAINBOW 5
-#define MODES_NUMBER 5
-#define MODE_RANDOM 6
-
-// beating
-//#define BEAT_AMP 0.08
 #define BEAT_FREQ 0.5
-#define BLINK_TIMES 4
-#define CIRCLE_FREQ 0.25
+// #define BLINK_TIMES 4
+// #define CIRCLE_FREQ 0.25
 
-const int R1_N = 2;
-const float R1_AMPS[] = {0.5, 0.5};
-const float R1_FREQS[] = {0.0, 0.05};
-const float R1_PHASES[] = {0.0, PI};
 
-//#define R1_N  2
-//#define R1_AMPS (0.5f,-0.5f)
-//#define R1_FREQS (0.0f,0.05f)
-//#define R1_PHASES (0.0f,0.0f)
+static const NPeriodicData<2> RAINBOW1_PD = {Functions::COSINES, 2, {0.5, -0.5}, {0, 0.05}, {0.0, PI}};
+static const NPeriodicData<3> RAINBOW2_PD = {Functions::COSINES,3, {0.25, 0.5, 0.25}, {0, 0.05, 0.4}, {0.0, 0.0, 0.0}};
+static const NPeriodicData<2> FAST_RAINBOW_PD = {Functions::COSINES,2, {0.5, 0.5}, {0, 0.4}, {0.0, PI}};
+// static const PeriodicDataT<2> CIRCLE_RAINBOW_PD = {Functions::CIRCLES,2, {0.9, 0.1}, {0.1, 1}, {0.0, 0}};
+static const NPeriodicData<2> CIRCLE_RAINBOW_PD = {Functions::CIRCLES,1, {1}, {0.1}, {0}};
+static const NPeriodicData<2> BEAT_PD = {Functions::COSINES,2, {0.5, 0.5}, {0, BEAT_FREQ}, {0.0, PI}};
 
-const int R2_N = 3;
-const float R2_AMPS[] = {0.25, 0.5, 0.5};
-const float R2_FREQS[] = {0.0, 0.05, 0.4};
-const float R2_PHASES[] = {0.0, 0.0, 0.0};
+class AnimationFunctions : public Functions
+{
 
-const int RF_N = 2;
-const float RF_AMPS[] = {0.5, 0.5};
-const float RF_FREQS[] = {0.0, 0.4};
-const float RF_PHASES[] = {0.0, PI};
+	// NON-BLOCKING METHODS
+public:
+	static const byte MIN_ANIMATION_ID = 128;
+	typedef enum
+	{
+		BEATING = MIN_ANIMATION_ID,
+		RAINBOW1,
+		RAINBOW2,
+		FAST_RAINBOW,
+		CIRCLE_RAINBOW
+	} FunctionTypes;
 
-const int RC_N = 1;
-const float RC_AMPS[] = {1};
-const float RC_FREQS[] = {0.25};
-const float RC_PHASES[] = {0.0};
+	float amp;
+	bool positive;
+	byte program;
 
-const int BEAT_N = 2;
-const float BEAT_AMPS[] = {0.5, 0.5};
-const float BEAT_FREQS[] = {0, BEAT_FREQ};
-const float BEAT_PHASES[] = {0.0, PI};
+	AnimationFunctions() : Functions()
+	{
+		positive = false;
+		program = NO_FUNCTION;
+	}
+	// ~AnimationFunctions(){
 
-// these are the pins connected to
-// the RGB LEDs
+	// }
+	float beat()
+	{
+		// return amp * sinePulse(1, 0, 0.25);
+		return amp * cosines(BEAT_PD.size, (float *)BEAT_PD.amps, (float *)BEAT_PD.freqs, (float *)BEAT_PD.phases);
+	}
 
+	float rainbow1()
+	{
+		// return amp * cosines(R1_N, (float *)R1_AMPS, (float *)R1_FREQS, (float *)R1_PHASES);
+		return amp * cosines(RAINBOW1_PD.size, (float *)RAINBOW1_PD.amps, (float *)RAINBOW1_PD.freqs, (float *)RAINBOW1_PD.phases);
+	}
+
+	float rainbow2()
+	{
+		// return amp * cosines(R2_N, (float *)R2_AMPS, (float *)R2_FREQS, (float *)R2_PHASES);
+		return amp * cosines(RAINBOW2_PD.size, (float *)RAINBOW2_PD.amps, (float *)RAINBOW2_PD.freqs, (float *)RAINBOW2_PD.phases);
+	}
+
+	float fastRainbow()
+	{
+		// return amp * cosines(RF_N, (float *)RF_AMPS, (float *)RF_FREQS, (float *)RF_PHASES);
+		return amp * cosines(FAST_RAINBOW_PD.size, (float *)FAST_RAINBOW_PD.amps, (float *)FAST_RAINBOW_PD.freqs, (float *)FAST_RAINBOW_PD.phases);
+	}
+
+	float circleRainbow()
+	{
+		// return amp * circles(RC_N, (float *)RC_AMPS, (float *)RC_FREQS, (float *)RC_PHASES);
+		return amp * cosines(CIRCLE_RAINBOW_PD.size, (float *)CIRCLE_RAINBOW_PD.amps, (float *)CIRCLE_RAINBOW_PD.freqs, (float *)CIRCLE_RAINBOW_PD.phases);
+	}
+
+	void setProgram(unsigned int p)
+
+	{
+		program = p;
+
+		if (program < MIN_ANIMATION_ID)
+		{
+			setFunctionType(program);
+		}
+		else
+		{
+
+			switch (program)
+			{
+			case BEATING:
+				setFunctionType(BEAT_PD.functionType);
+				setPeriodicData(BEAT_PD.size, (float *)BEAT_PD.amps, (float *)BEAT_PD.freqs, (float *)BEAT_PD.phases);
+
+				break;
+			case RAINBOW1:
+				setFunctionType(RAINBOW1_PD.functionType);
+				setPeriodicData(RAINBOW1_PD.size, (float *)RAINBOW1_PD.amps, (float *)RAINBOW1_PD.freqs, (float *)RAINBOW1_PD.phases);
+
+				break;
+			case RAINBOW2:
+				setFunctionType(RAINBOW2_PD.functionType);
+				setPeriodicData(RAINBOW2_PD.size, (float *)RAINBOW2_PD.amps, (float *)RAINBOW2_PD.freqs, (float *)RAINBOW2_PD.phases);
+
+				break;
+			case FAST_RAINBOW:
+				setFunctionType(FAST_RAINBOW_PD.functionType);
+				setPeriodicData(FAST_RAINBOW_PD.size, (float *)FAST_RAINBOW_PD.amps, (float *)FAST_RAINBOW_PD.freqs, (float *)FAST_RAINBOW_PD.phases);
+
+				break;
+			case CIRCLE_RAINBOW:
+				setFunctionType(CIRCLE_RAINBOW_PD.functionType);
+				setPeriodicData(CIRCLE_RAINBOW_PD.size, (float *)CIRCLE_RAINBOW_PD.amps, (float *)CIRCLE_RAINBOW_PD.freqs, (float *)CIRCLE_RAINBOW_PD.phases);
+
+				break;
+
+			default:
+				break;
+			}
+		}
+	}
+
+	float output()
+	{
+		return amp * (positive ? positiveWave(value()) : value());
+	}
+
+	bool isAnimating(){
+		return program > 0;
+	}
+};
 class ColorAnimation : public Color
 {
 
 public:
-	Functions f;
-
-	byte brightnessAnimation;
+	AnimationFunctions brightnessAnimation;
 	
-	byte colorAnimation;
+	AnimationFunctions hueAnimation;
+	
 
-	byte animationMode;
+	// byte animationMode;
 
 	uint16_t _prevHue;
 
+	ColorAnimation() : Color(), brightnessAnimation(), hueAnimation()
+	{
+		brightnessAnimation.amp = BRIGHTNESS_MAX;
+		hueAnimation.amp = HUE_MAX;
 
-	ColorAnimation() : Color(), f() {}
+		brightnessAnimation.speed = hueAnimation.speed = 1;
+	}
 	virtual ~ColorAnimation() {}
 
 	Color *color() { return (Color *)this; }
-	void resetEffectsTimer() { f.resetTimer(); }
+
+	void resetEffectsTimers()
+	{
+		brightnessAnimation.resetTimer();
+		hueAnimation.resetTimer();
+	}
+
+	void setBrightnessAnimation(byte animation)
+	{
+		brightnessAnimation.setProgram(animation);
+		if (animation == NO_ANIMATION)
+		{
+
+			setBrightness(BRIGHTNESS_MAX);
+		}
+		else
+		{
+			brightnessAnimation.resetTimer();
+		}
+	}
+
+	void setColorAnimation(byte animation)
+	{
+		hueAnimation.setProgram(animation);
+		if (animation == NO_ANIMATION)
+		{
+
+			setHue(_prevHue);
+		}
+		else
+		{
+
+			_prevHue = _hue;
+			hueAnimation.resetTimer();
+		}
+	}
 
 	void setAnimation(byte animation)
 	{
 
 		if (animation == NO_ANIMATION)
 		{
-			brightnessAnimation = animationMode = animation;
+			setColorAnimation(NO_ANIMATION);
+			setBrightnessAnimation(NO_ANIMATION);
 		}
-		else if (animation == MODE_BEATING)
+		else if (animation == AnimationFunctions::BEATING)
 		{
 
-			brightnessAnimation = (brightnessAnimation == MODE_BEATING) ? NO_ANIMATION : MODE_BEATING;
+			setBrightnessAnimation(AnimationFunctions::BEATING);
 		}
 		else
 		{
-			animationMode = animation;
-			_prevHue = _hue;
-			f.resetTimer();
+			setColorAnimation(animation);
 		}
-
-		
 	}
 	bool beatColor2Zero()
 	{
-		beatColor();
+		setBrightness(brightnessAnimation.beat());
 		return (_brightness > 1);
 	}
 
@@ -118,93 +231,104 @@ public:
 	//		}while (brightness>1);
 	//	}
 
-	// NON-BLOCKING METHODS
+	// void beatColor(/*float timesWT = 6*/)
+	// {
 
-	void beatColor(/*float timesWT = 6*/)
-	{
+	// 	float brightness = 255 * brightnessAnimation.cosines(BEAT_N, (float *)BEAT_AMPS, (float *)BEAT_FREQS, (float *)BEAT_PHASES, 2.0);
+	// 	setBrightness(brightness);
+	// }
 
-		float brightness = 255 * f.cosines(BEAT_N, (float *)BEAT_AMPS, (float *)BEAT_FREQS, (float *)BEAT_PHASES,2.0);
-		setBrightness(brightness);
-	}
+	// void circleRainbow(uint16_t center, float freq)
+	// {
+	// 	//		float hue = circle(CIRCLE_FREQ);
+	// 	float circle = hueAnimation.circles(RC_N, (float *)RC_AMPS, (float *)RC_FREQS, (float *)RC_PHASES);
+	// 	uint16_t hue = uint16_t(center + (float(HUE_MAX) * circle)) % HUE_MAX;
 
-	void rainbow1()
-	{
-		float hue = HUE_MAX * f.cosines(R1_N, (float *)R1_AMPS, (float *)R1_FREQS, (float *)R1_PHASES);
-		// setHSB(hue_in_range(hue+_prevHue),SATURATION_MAX,BRIGHTNESS_MAX);
-		setHue(hue_in_range(hue + _prevHue));
-	}
-
-	void rainbow2()
-	{
-		float hue = HUE_MAX * f.cosines(R2_N, (float *)R2_AMPS, (float *)R2_FREQS, (float *)R2_PHASES);
-		//   setHSB(hue_in_range(hue+_prevHue),SATURATION_MAX,BRIGHTNESS_MAX);
-		setHue(hue_in_range(hue + _prevHue));
-	}
-
-	void fastRainbow()
-	{
-		float hue = HUE_MAX * f.cosines(RF_N, (float *)RF_AMPS, (float *)RF_FREQS, (float *)RF_PHASES);
-		// setHSB(hue_in_range(hue+_prevHue),SATURATION_MAX,BRIGHTNESS_MAX);
-		setHue(hue_in_range(hue + _prevHue));
-	}
-
-	void circleRainbow()
-	{
-		float hue = HUE_MAX * f.circles(RC_N, (float *)RC_AMPS, (float *)RC_FREQS, (float *)RC_PHASES);
-		// Serial.println(hue);
-		// setHSB(hue_in_range(hue+_prevHue),SATURATION_MAX,BRIGHTNESS_MAX);
-		setHue(hue_in_range(hue + _prevHue));
-	}
-
-	void circleRainbow(uint16_t center, float freq)
-	{
-		//		float hue = circle(CIRCLE_FREQ);
-		float circle = f.circles(RC_N, (float *)RC_AMPS, (float *)RC_FREQS, (float *)RC_PHASES);
-		uint16_t hue = uint16_t(center + (float(HUE_MAX) * circle)) % HUE_MAX;
-
-		// setHSB(hue_in_range(hue+_prevHue),SATURATION_MAX,BRIGHTNESS_MAX);
-		setHue(hue_in_range(hue + _prevHue));
-	}
+	// 	// setHSB(hue_in_range(hue+_prevHue),SATURATION_MAX,BRIGHTNESS_MAX);
+	// 	setHue(hue + _prevHue);
+	// }
 
 	void allRandom()
 	{
 		setRGB(random(0, 255), random(0, 255), random(0, 255));
 	}
 
+	// void 	beatColor(){
+	// 		setBrightness(brightnessAnimation.beat());
+	// }
 	void updateAnimation()
 	{
 
-		switch (brightnessAnimation)
-		{
-		case MODE_BEATING:
-			beatColor();
-		
-		default:
-			break;
+		uint16_t h = _prevHue;
+		if(hueAnimation.isAnimating()){
+			h+= hueAnimation.output();
 		}
-	
+		uint8_t b = brightnessAnimation.isAnimating() ? (uint8_t)brightnessAnimation.output() : BRIGHTNESS_MAX;
+		uint8_t s = SATURATION_MAX;
 
-	switch (animationMode)
-	{
-	case MODE_RAINBOW1:
-		rainbow1();
-		break;
-	case MODE_RAINBOW2:
-		rainbow2();
-		break;
-	case MODE_FAST_RAINBOW:
-		fastRainbow();
-		break;
-	case MODE_CIRCLE_RAINBOW:
-		circleRainbow();
-		break;
-	case MODE_RANDOM:
-		allRandom();
-	default:
-		break;
+// if(brightnessAnimation.isAnimating())
+// 		switch (brightnessAnimation)
+// 		{
+// 		case MODE_BEATING:
+// 			b = brightnessAnimation.beat();
+// 			// setBrightness(brightnessAnimation.beat());
+// 			// beatColor();
+// 			break;
+// 		default:
+// 			// setBrightness(BRIGHTNESS_MAX);
+// 			break;
+// 		}
+
+// 		switch (colorAnimation)
+// 		{
+// 		// case MODE_BEATING:
+// 		// 	setHue(_prevHue + hue_in_range(hueAnimation.beat()));
+// 		// 	break;
+// 		case MODE_RAINBOW1:
+
+// 			h += hueAnimation.rainbow1();
+// 			// setHue(hueAnimation.rainbow1() + _prevHue);
+// 			// setHue(_prevHue + hueAnimation.rainbow1());
+// 			break;
+// 		case MODE_RAINBOW2:
+// 			h += hueAnimation.rainbow2();
+// 			// setHue(hueAnimation.rainbow2() + _prevHue);
+// 			// setHue(_prevHue + hueAnimation.rainbow2());
+// 			break;
+// 		case MODE_FAST_RAINBOW:
+// 			h += hueAnimation.fastRainbow();
+// 			// setHue(hueAnimation.fastRainbow() + _prevHue);
+// 			// setHue(_prevHue + hueAnimation.fastRainbow());
+// 			break;
+// 		case MODE_CIRCLE_RAINBOW:
+// 			h += hueAnimation.circleRainbow();
+// 			// setHue(hueAnimation.circleRainbow() + _prevHue);
+// 			// Serial.println(hue());
+// 			break;
+// 		// case MODE_RANDOM:
+// 		// 	allRandom();
+// 		// break;
+// 		default:
+// 			break;
+// 		}
+
+		setHSV(h, s, b);
 	}
-}
-}
-;
+
+	bool isBrightnessAnimating()
+	{
+		return brightnessAnimation.isAnimating();
+	}
+
+	bool isColorAnimating()
+	{
+		return hueAnimation.isAnimating();
+	}
+
+	bool isAnimating()
+	{
+		return isBrightnessAnimating() || isColorAnimating();
+	}
+};
 
 #endif /* COLOR_ANIMATION_H_ */
